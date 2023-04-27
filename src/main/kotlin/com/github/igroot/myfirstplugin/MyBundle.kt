86d0c3a93d23e205515168c497734b90d3db28d8
GitHub Action <action@github.com>
2023-04-27 12:14:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/igroot/myfirstplugin/MyBundle.kt
+++ src/main/kotlin/com/github/igroot/myfirstplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.igroot.myfirstplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/igroot/myfirstplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/igroot/myfirstplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.igroot.myfirstplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.igroot.myfirstplugin.MyBundle
+import com.github.igroot.myfirstplugin.services.MyProjectService
 import javax.swing.JButton
 
 
